1f13302d47a6fcaccb4af900e311f0624e233389
Kevin Chevalier <kevin.chevalier10@gmail.com>
2020-10-18 22:17:56 -1000

    Upgraded to unity version to 2019.4.12f.1 and added weapon class

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿
+public class Weapon{
+    public float damageMultiplier=1f;
+    public int damageBonus=0;
+    public float healthMultiplier=1f;
+    public int healthBonus=0;
+    public float speedMultiplier=1f;
+    public int speedBonus=0;
+    public float reloadMultiplier=1f;
+    public float attackRateMultiplier=1f;
+    public int uses=-1;
+    public bool infiniteUse = true;
+    public int cost = 0;
+    public float effectTime=0f;
+    public int impactRange=0;
+}
